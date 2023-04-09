1d4a1b99ef90b9023cb9b781296e8f13038cf454
Saleh Nasim <nasimcse16@gmail.com>
2023-04-10 00:27:27 +0600

    react-router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Navbar from './components/Navbar'
+import Blog from "./components/Blog"
+import Statistics from "./components/Statistics"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    errorElement: <h1>Hello Error page</h1>,
+    children:[
+      {
+        path: "blog",
+        element: <Blog />
+      },
+      {
+        path: "statistics",
+        element: <Statistics />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
