0b06914576604a127d50c2c3228be58375bfe9a3
Saleh Nasim <nasimcse16@gmail.com>
2023-04-10 20:08:31 +0600

    statistics page done

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
-  const {marks} = useLoaderData();
+  const { marks } = useLoaderData();
   console.log(marks);
   return (
-    <div>
-      <h1>Hello from statistics page</h1>
+    <div className='my-container'>
+      <ResponsiveContainer width={'99%'} height={300}>
+        <BarChart
+          width={500}
+          height={300}
+          data={marks}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          barSize={20}
+        >
+          <XAxis dataKey="assignment_no" scale="point" padding={{ left: 10, right: 10 }} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="marks" fill="#8884d8" background={{ fill: '#eee' }} />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
